a2f4e9e44d9ddaa6568dfcb2156cbbd06a9d1466
yakub499 <57057630+yakub499@users.noreply.github.com>
2022-01-25 22:03:31 -0800

    Update first.py
    change

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
+import os
+
+Name=os.getenv("Name")
 def test():
-    print("This is first python code for test")
+    print("This is first python code for test" + Name)
 def test2():
     print("this second function")
 test()
