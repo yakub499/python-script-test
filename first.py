8d569d2b91e31f634c56b5b7f2458e087e13d096
yakub499 <57057630+yakub499@users.noreply.github.com>
2022-02-06 20:51:16 -0800

    Update first.py
    test

--- first.py
+++ first.py
@@ -1,8 +1,10 @@
 import os
 
 Name=os.getenv("Name")
+db_name1=os.getenv("db_name")
 def test():
     print("This is first python code for test" + Name)
+    print(db_name1)
 def test2():
     print("this second function")
 test()
